bc93a5cf5d790d21a845cf80f08445d24714d671
jhormanf26 <82169503+jhormanf26@users.noreply.github.com>
2022-09-01 22:35:45 -0500

    Formula para hallar el Área esfera

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 
 
-let imagen="imagenes/Circulo.png";
-let a = Number(prompt(`Ingrese radio  del Circulo`,1));
+let imagen="imagenes/Esfera.png";
+let a = Number(prompt(`Ingrese radio  de la Esfera`,1));
 
 
-function areaCirculo(radio){
-    return Math.PI*radio*radio;
+function areaEsfera(radio){
+    return 4*Math.PI*radio*radio;
 }
 
 function imprimir(titulo , texto){
@@ -13,5 +13,5 @@ function imprimir(titulo , texto){
         console.log(texto)
     console.groupEnd();
 }
-imprimir(`El area del Circulo :`,`${areaCirculo(a)}`);
+imprimir(`El area del Esfera :`,`${areaEsfera(a)}`);
 document.body.insertAdjacentHTML("afterbegin",`<img src="${imagen}">`);
