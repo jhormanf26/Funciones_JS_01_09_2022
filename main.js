ef7b7bb70e57c1a2dba27a4e12fb2f9ec78ef501
jhormanf26 <82169503+jhormanf26@users.noreply.github.com>
2022-09-01 22:12:39 -0500

    Formula para hallar el Área del trapecio

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 
 
-let imagen="imagenes/Rombo.png";
-let a = Number(prompt(`Ingrese la diagonal mayor del Rombo`,1));
-let b = Number(prompt(`Ingrese la diagonal menor del Rombo`,1));
+let imagen="imagenes/Trapecio.png";
+let a = Number(prompt(`Ingrese la base mayor del Trapecio`,1));
+let b = Number(prompt(`Ingrese la base menor del Trapecio`,1));
+let c = Number(prompt(`Ingrese la altura del Trapecio`,1));
 
-function areaRombo(diagonalMenor ,diagonalMayor){
-    return (diagonalMayor*diagonalMenor)/2;
+
+function areaTrapecio(baseMenor ,baseMayor,altura){
+    return ((baseMayor+baseMenor)/2)*altura;
 }
 
 function imprimir(titulo , texto){
@@ -13,5 +15,5 @@ function imprimir(titulo , texto){
         console.log(texto)
     console.groupEnd();
 }
-imprimir(`El area del Rombo :`,`${areaRombo(b,a)}`);
+imprimir(`El area del Trapecio :`,`${areaTrapecio(b,a,c)}`);
 document.body.insertAdjacentHTML("afterbegin",`<img src="${imagen}">`);
