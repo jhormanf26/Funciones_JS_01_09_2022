73c8eefa6ee9a5390e4f4f7f36568d93f2538b9d
jhormanf26 <82169503+jhormanf26@users.noreply.github.com>
2022-09-02 23:23:56 -0500

    Formula para hallar el volumen dela esfera

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 
 
-let imagen="imagenes/Cilindro.png";
-let a = Number(prompt(`Ingrese el radio de la base del Cilindro`,1));
-let b = Number(prompt(`Ingrese el alto del Cilindro`,1));
+let imagen="imagenes/Esfera.png";
+let a = Number(prompt(`Ingrese el radio de la Esfera`,1));
 
-function volumenCilindro(radio,alto){
-    return Math.PI*radio*radio*alto;
+function volumenEsfera(radio){
+    return Math.PI*radio*radio*radio*4/3;
 }
 
 function imprimir(titulo , texto){
@@ -13,5 +12,5 @@ function imprimir(titulo , texto){
         console.log(texto)
     console.groupEnd();
 }
-imprimir(`El Volumen  del Cilindro :`,`${volumenCilindro(a,b)}`);
+imprimir(`El Volumen  del Esfera :`,`${volumenEsfera(a)}`);
 document.body.insertAdjacentHTML("afterbegin",`<img src="${imagen}">`);
