25738162d7f8e4bb5804da742c338b635e7c6443
jhormanf26 <82169503+jhormanf26@users.noreply.github.com>
2022-09-02 23:21:03 -0500

    Formula para hallar el volumen del cilindro

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 
 
-let imagen="imagenes/Cono.png";
-let a = Number(prompt(`Ingrese el area de la base del Cono`,1));
-let b = Number(prompt(`Ingrese el alto del Cono`,1));
+let imagen="imagenes/Cilindro.png";
+let a = Number(prompt(`Ingrese el radio de la base del Cilindro`,1));
+let b = Number(prompt(`Ingrese el alto del Cilindro`,1));
 
-function volumenCono(areaBase,alto){
-    return (areaBase/3)*alto;
+function volumenCilindro(radio,alto){
+    return Math.PI*radio*radio*alto;
 }
 
 function imprimir(titulo , texto){
@@ -13,5 +13,5 @@ function imprimir(titulo , texto){
         console.log(texto)
     console.groupEnd();
 }
-imprimir(`El Volumen  del Cono :`,`${volumenCono(a,b)}`);
+imprimir(`El Volumen  del Cilindro :`,`${volumenCilindro(a,b)}`);
 document.body.insertAdjacentHTML("afterbegin",`<img src="${imagen}">`);
